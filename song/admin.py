6a1fe2f9caac836c76bb8a64924780923d1fd3b0
Alisa <alisa.barsky@gmail.com>
2018-03-05 17:19:22 +0200

    last changes

--- song/admin.py
+++ song/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Songs, Words, WordsOfSongs
 
-admin.site.register(Songs)
-admin.site.register(Words)
-admin.site.register(WordsOfSongs)
+#admin.site.register(Songs)
+#admin.site.register(Words)
+#admin.site.register(WordsOfSongs)
 # Register your models here.
+class SongsAdmin(admin.ModelAdmin):
+    fields = ['song_name', 'author_name' , 'song_url']
+admin.site.register(Songs, SongsAdmin)
+
+class WordsOfSongsAdmin(admin.ModelAdmin):
+    fields = ['words', 'songs' , 'num_songs']
+admin.site.register(WordsOfSongs, WordsOfSongsAdmin)
+
+class WordsAdmin(admin.ModelAdmin):
+    fields = ['word', 'num_docs'] 
+admin.site.register(Words, WordsAdmin)
