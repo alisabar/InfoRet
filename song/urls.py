0d77965ecc1d1d9496941a19e94dd678bb06cb82
Alisa <alisa.barsky@gmail.com>
2018-03-15 11:36:00 +0200

    delete works

--- song/urls.py
+++ song/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('newfile/', views.create_tables, name='newfile'),
+
+    path('allsongs/', views.view, name='view'),
+    
+    path('wasdeleted/', views.wasdeleted, name='wasdeleted'),
     # ex: /song/5/
     path('<int:song_id>/detail/', views.detail, name='detail'),
     # ex: /song/5/words/
--- song/views.py
+++ song/views.py
@@ -6,6 +6,7 @@ from lxml import html
 import re
 import requests
 import logging
+from bs4 import BeautifulSoup
 from django.utils.datastructures import MultiValueDictKeyError
 # Create your views here.
 
@@ -15,28 +16,54 @@ def index(request):
     return render(request,'song/index.html')
 
 def detail(request,song_id):
-    songdetails = get_object_or_404(Songs, pk=song_id)
+    try:
+        document = Songs.objects.get(pk=song_id)
+    except Songs.DoesNotExist:
+        raise Http404("Document does not exist")
 
-    #  songdetails = Songs.objects.get(pk=song_id)
-  
+    return render(request, 'song/detail.html', {'document': document})
 
- 
-    return render(request,'song/detail.html',{ 'content':detail, 'songdetails':songdetails  })
+def view(request):
+    document_list=Songs.objects.all()
+    context = {'document_list': document_list}
+    return render(request,'song/allsongs.html',context)
+
+def detele_document(song_id):
+    
+    print ('detele_document '+  str(song_id))
+    
+    document = get_object_or_404(Songs, pk=song_id)
+    document.delete()
+    
+
+def wasdeleted(request):
+    #import pdb; pdb.set_trace()
+    doc_list=(request.POST['slected_document_ids']).split(",")
+    for doc in doc_list:
+        detele_document(int(doc))
+    
+
+
+    #doc_list=request.POST['docs_list']
+    #for doc in doc_list:
+    #    detele_document(doc.id)
+
+    return render(request,'song/wasdeleted.html')
 
 def create_tables(request):
     
     try:
         url=request.POST['link']
         detail=parse_url(url)
-        author=""
+        author="Beatles"
         stop_words=['a href',]
         detail_title=detail['title']
         detail_body=detail['body']
         sentence=''
         mid=''
         if not (detail_title is None):
-            for x in range(len(detail_title)):
-                mid=detail_title[x]
+            #for x in range(len(detail_title)):
+                mid=detail_title
                 mid=mid.lower()
                 mid= mid.replace('.','').replace('!','').replace(',','').replace('?','').replace('(','').replace(')','').replace('-','').replace('*','')
                 sentence= sentence+' '+mid
@@ -46,11 +73,11 @@ def create_tables(request):
 
         if not (detail_body is None):
         
-            for x in range(len(detail_body)):
-                mid=detail_body[x]
-                mid= mid.lower()
+           # for x in range(len(detail_body)):
+                mid2=detail_body
+                mid22= mid.lower()
                 mid= mid.replace('.','').replace('!','').replace(',','').replace('?','').replace('(','').replace(')','').replace('-','').replace('*','')
-                sentence= sentence+' '+mid
+                sentence= sentence+' '+mid2
         else:
             w="no body"
 
@@ -81,18 +108,21 @@ def create_tables(request):
 
     return render(request,'song/newfile.html',context)
 
+
 def parse_url(song_url):
  
-   # song = Songs.objects.get(pk=song_id)
-   # url=song.song_url('https://en.wikipedia.org/wiki/Hayim_Nahman_Bialik')
+
     page = requests.get(song_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+  #  tree = html.fromstring(page.content)
 
-    tree = html.fromstring(page.content)
+    author_song_name=soup.find(class_='lyric-song-head').get_text()
+    body=soup.find(class_='dn').get_text()
 
-    title = tree.xpath('//h1[@class="firstHeading"]/text()')
-    body= tree.xpath('//div[@class="mw-parser-output"]/p[14]/text()')
+    #title = tree.xpath('//h1[@class="firstHeading"]/text()')
+   # body= tree.xpath('//div[@class="mw-parser-output"]/p[14]/text()')
     context = {
-        'title': title,
+        'title': author_song_name,
         'body': body,
     }
     logger= logging.getLogger(__name__)
